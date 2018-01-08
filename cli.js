1c80025dbbc13f815fdd4a27754133046559c17d
Blealtan <blealtan@gmail.com>
2018-01-08 19:09:57 +0800

    Finish cli tool.

--- cli.js
+++ cli.js
@@ -1,9 +1,18 @@
+import {
+  transform
+} from '.';
 
-import * as tmips from './index'
-var argv = require('minimist')(process.argv.slice(2))
+var argv = require('minimist')(process.argv.slice(2), {
+  'boolean': true
+})
+const promisify = require('util').promisify
+const fs = require('fs')
+const readFile = promisify(fs.readFile),
+  writeFile = promisify(fs.writeFile)
 
 if (argv.h || argv.help) {
-  console.log('Usage: node cli.js <input-mips32> -o <output-tmips>.')
+  console.log('Usage: node cli.js <input-mips32> [-o <output-tmips>] [--pretty].')
+  console.log('    If -o isn\'t provided, it will be treated as -o <input-mips32>.s.')
   process.exit(0)
 }
 
@@ -15,4 +24,13 @@ if (argv._.length !== 1) {
 var input = argv._[0]
 var output = 'o' in argv ? argv.o : input + '.s'
 
-//! TODO: read input, process thru tmips, and write output.
+(async() =>
+  await writeFile(output,
+    (await readFile(input))
+    .toString('ascii')
+    .split('\n')
+    .map(transform)
+    .reduce((acc, val) => acc.concat(val))
+    .join('\n')
+  )
+)()
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
-function transformLine (line) {
+function transformLine(line) {
   let transformedTMips = []
   //! TODO: do actual transforming stuff.
   return transformedTMips
 }
 
-function transform (lines) {
-  return lines.forEach(transformLine)
+function transform(lines) {
+  return lines.map(elem => ({"src": elem, "res": transformLine(elem)}))
 }
 
 exports.transform = transform
